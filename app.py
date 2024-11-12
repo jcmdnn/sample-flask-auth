eb1c9855173d1cb1e0a2c33520da65bbc98ce151
João Carlito <joaocarlitoneto@gmail.com>
2024-11-12 17:32:40 -0300

    create route login

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mysecretkey"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        return jsonify({"message": "Autenticação realizada com sucesso"})
+    
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
 @app.route("/hello-world", methods=['GET'])
 def hello_world():
